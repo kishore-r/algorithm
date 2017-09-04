310a3289e1df4793d5b67345ed0bb19ad2c1f915
kishore-r <tookishore@hotmail.com>
2017-09-04 14:12:19 +0800

    added dijiktra logic , unit test files to test implementations

--- src/com/algorithm/graph/dijkstra/Dijkstra.java
+++ src/com/algorithm/graph/dijkstra/Dijkstra.java
@@ -13,6 +13,10 @@
  ************************************************************************/
 package com.algorithm.graph.dijkstra;
 
+import java.util.ArrayList;
+
+import com.algorithm.util.AlgorithamUtil;
+
 /**
  * COMMENT - Add description of this class or interface here. 
  *           Description should go beyond the class/interface name.
@@ -43,5 +47,78 @@ package com.algorithm.graph.dijkstra;
  *              
  */
 public class Dijkstra {
+    private int currentSource;
+    private Graph graph;
+    private int sourceNode;
 
+    public Dijkstra(Graph graph, int source) {
+        this.graph = graph;
+        this.currentSource = source;
+        this.sourceNode = source;
+    }
+
+    public void calculateShortestPath() {
+        // initialize the source distance to := 0
+        Node[] nodes = this.graph.getNodes();
+        nodes[currentSource].setDistanceFromSource(0);
+        for (int nodesIndex = 0; nodesIndex < nodes.length; nodesIndex++) {
+            ArrayList<Edge> currentEdgeList = nodes[this.currentSource].getEdges();
+            for (int edgeIndex = 0; edgeIndex < currentEdgeList.size(); edgeIndex++) {
+                int neighbour = currentEdgeList.get(edgeIndex).getNebhourIndex(currentSource);
+                if (!nodes[neighbour].isVisited()) {
+                    int calculatedDistance = nodes[this.currentSource].getDistanceFromSource() + currentEdgeList.get(edgeIndex).getLength();
+
+                    if (calculatedDistance < nodes[neighbour].getDistanceFromSource()) {
+                        nodes[neighbour].setDistanceFromSource(calculatedDistance);
+                    }
+
+                }
+            }
+            nodes[this.currentSource].setVisited(true);
+            this.currentSource = getNextShortestDistancedNode(nodes);
+        }
+        displayShortestPath();
+    }
+
+    private int getNextShortestDistancedNode(Node[] nodes) {
+        int tmpNodeIndex = 0;
+        int tmpShotestDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < nodes.length; i++) {
+            int currentDistance = nodes[i].getDistanceFromSource();
+            if (!nodes[i].isVisited() && currentDistance < tmpShotestDistance) {
+                tmpShotestDistance = currentDistance;
+                tmpNodeIndex = i;
+            }
+        }
+        return tmpNodeIndex;
+    }
+
+    private void displayShortestPath() {
+        Node[] nodes = this.graph.getNodes();
+        AlgorithamUtil.println("--------------------------------------------");
+        for (int i = 0; i < nodes.length; i++) {
+            AlgorithamUtil.println("Shortest Distance from Node " + this.sourceNode + " to Node " + i + " is : " + nodes[i].getDistanceFromSource());
+        }
+
+    }
+
+    /**
+     * COMMENT - Add concise description of this setter method.
+     *           Description should go beyond the method name.
+     * 
+     * @return Returns the graph.
+     */
+    public Graph getGraph() {
+        return graph;
+    }
+
+    /**
+     * COMMENT - Add concise description of this setter method.
+     *           Description should go beyond the method name.
+     * 
+     * @param graph The graph to set.
+     */
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
 }
---
+++ srctest/com/algorithm/graph/dijkstra/TestDijikstra.java
@@ -0,0 +1,39 @@
+/************************************************************************
+ *                                                                      *
+ *  DDDD     SSSS    AAA        Daten- und Systemtechnik Aachen GmbH    *
+ *  D   D   SS      A   A       Pascalstrasse 28                        *
+ *  D   D    SSS    AAAAA       52076 Aachen-Oberforstbach, Germany     *
+ *  D   D      SS   A   A       Telefon: +49 (0)2408 / 9492-0           *
+ *  DDDD    SSSS    A   A       Telefax: +49 (0)2408 / 9492-92          *
+ *                                                                      *
+ *                                                                      *
+ *  (c) Copyright by DSA - all rights reserved                          *
+ *                                                                      *
+ ************************************************************************
+ *
+ * Initial Creation:
+ *    Author      rak
+ *    Created on  Sep 4, 2017
+ *
+ ************************************************************************/
+package com.algorithm.graph.dijkstra;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestDijikstra {
+
+    @Test
+    public void testCalculateShortestPath() {
+        Graph graph = new Graph(new Edge[] { new Edge(0, 2, 1), new Edge(0, 3, 4), new Edge(0, 4, 2), new Edge(0, 1, 3), new Edge(1, 3, 2), new Edge(1, 4, 3), new Edge(1, 5, 1), new Edge(2, 4, 1), new Edge(3, 5, 4), new Edge(4, 5, 2),
+            new Edge(4, 6, 7), new Edge(4, 7, 2), new Edge(5, 6, 4), new Edge(6, 7, 5) });
+        graph.printGraph();
+        Dijkstra dijiktra = new Dijkstra(graph, 0);
+        dijiktra.calculateShortestPath();
+        assertTrue(dijiktra.getGraph().getNodes()[5].getDistanceFromSource() == 4);
+        assertTrue(dijiktra.getGraph().getNodes()[6].getDistanceFromSource() == 8);
+        assertTrue(dijiktra.getGraph().getNodes()[7].getDistanceFromSource() == 4);
+    }
+
+}
